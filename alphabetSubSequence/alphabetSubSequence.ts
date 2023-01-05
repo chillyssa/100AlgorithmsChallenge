cb7ba89b4b107b3bbe2728e58051c337f7142939
Alyssa Williams <38333607+chillyssa@users.noreply.github.com>
2023-01-05 15:37:25 -0700

    completed algorithms 9 and 10.

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,25 @@
 export function alphabetSubsequence(s: string): boolean {
+    const chars: string[]= s.split('');
+    const charVals: number[] = []; 
 
+    // split the input array s into characters then push the letter's ascii value into another array
+    chars.forEach((char:string) =>{
+        charVals.push(char.charCodeAt(0))
+    })
+
+    // Set is like an array but doesn't take in duplicates - if the set from the charVals is not equal to the original array, there is a duplicate. 
+    if(new Set(charVals).size != charVals.length){
+        return false;
+    }
+
+    // Check that the characters are in alphabetic order 
+    for(let i=0; i<charVals.length; i++){
+        if(charVals[i]>= charVals[i+1]){
+            return false;
+        }
+    }
+
+    return true 
 }
 
 console.log(alphabetSubsequence('zab'))
@@ -7,3 +27,4 @@ console.log(alphabetSubsequence('effg'))
 console.log(alphabetSubsequence('cdce'))
 console.log(alphabetSubsequence('ace'))
 console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence('zbx'))
--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,26 @@
 export function alternatingSums(a: number[]): number[] {
+    const teamWeights: number[] = []; 
+    const team1: number[] = []; 
+    const team2: number[] = [];
+    let weight: number = 0; 
 
+    for(let i=0; i<a.length; i++){
+        if(i % 2 === 0 ){
+            team1.push(a[i])
+        } else{
+            team2.push(a[i])
+        }
+    }
+
+    
+    teamWeights.push(team1.reduce((accumulator, currentValue) => accumulator + currentValue,
+    weight));
+
+    teamWeights.push(team2.reduce((accumulator, currentValue) => accumulator + currentValue,
+    weight));
+
+
+    return teamWeights
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
